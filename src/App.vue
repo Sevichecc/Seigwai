8bfa655af592490661de722c13096d0f210a31ee
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-03-23 20:13:01 +0800

    Add Milkdown editor

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import Milkdown from './components/Milkdown/MilkdownEditor.vue'
+import { MilkdownProvider } from '@milkdown/vue';
+</script>
+
 <template>
-  <h2>App</h2>
+  <MilkdownProvider>
+    <Milkdown />
+  </MilkdownProvider>
 </template>
---
+++ src/components/Milkdown/MilkdownEditorWrapper.vue
@@ -0,0 +1,20 @@
+<script>
+import { defineComponent } from 'vue'
+import { MilkdownProvider } from '@milkdown/vue'
+
+export default defineComponent({
+  name: 'MilkdownEditorWrapper',
+  components: {
+    MilkdownProvider,
+    MilkdownEditor,
+  },
+  setup: () => {},
+})
+</script>
+
+<template>
+  <MilkdownProvider>
+    <MilkdownEditor />
+  </MilkdownProvider>
+</template>
+
