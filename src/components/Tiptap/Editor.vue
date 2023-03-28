4cf803356856c66d899d3fbe289cd37a4d02d903
Seviche <me@seviche.cc>
2023-03-28 22:20:44 +0800

    Refactor the style of editor

--- src/components/Tiptap/Editor.vue
+++ src/components/Tiptap/Editor.vue
@@ -17,39 +17,50 @@ const editor = useEditor({
       v-if="editor"
       :editor="editor"
       :tippy-options="{ duration: 100 }"
-      class="flex text-gray-700 bg-slate-50 rounded-md"
+      class="flex text-gray-700 bg-white grass rounded-md p-[2px] shadow-xl border-slate-100 border"
     >
       <button
-        class="btn btn-sm btn-ghost"
+        class="menu-btn"
+        placement="bold"
         @click="editor?.chain().focus().toggleBold().run()"
       >
-        bold
+        <span class="i-tabler-bold" />
       </button>
       <button
-        class="btn btn-sm btn-ghost"
+        class="menu-btn"
         @click="editor?.chain().focus().toggleItalic().run()"
       >
-        italic
+        <span class="i-tabler-italic" />
       </button>
       <button
-        class="btn btn-sm btn-ghost"
+        class="menu-btn"
         @click="editor?.chain().focus().toggleStrike().run()"
       >
-        strike
+        <span class="i-tabler-strikethrough" />
       </button>
       <button
-        class="btn btn-sm btn-ghost"
+        class="menu-btn"
         @click="editor?.chain().focus().toggleCode().run()"
       >
-        Code
+        <span class="i-tabler-code" />
       </button>
       <button
-        class="btn btn-sm btn-ghost"
+        class="menu-btn"
         @click="editor?.chain().focus().toggleBlockquote().run()"
       >
-        Quote
+        <span class="i-tabler-quote" />
       </button>
     </BubbleMenu>
     <EditorContent :editor="editor" />
   </div>
 </template>
+
+<style lang="pcss">
+.menu-btn {
+    @apply btn btn-ghost btn-sm hover:bg-slate-200 rounded-md p-2;
+}
+
+.menu-btn > span {
+  @apply w-5;
+}
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('daisyui'),
     iconsPlugin({
-      collections: getIconCollections(['mdi', 'lucide']),
+      collections: getIconCollections(['tabler']),
     }),
   ],
 }
